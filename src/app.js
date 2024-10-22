65ab8a3217454de1684eb79ae21a4f13fb3aedaa
Raphaël Beaugiraud <raphael.bgrd@gmail.com>
2024-10-22 17:29:38 +0200

    add CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const connect = require("./utils/connect");
 const swaggerDocs = require("./utils/swagger");
 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 const url = process.env.RENDER_EXTERNAL_URL || `http://localhost:${PORT}`;
 
 app.use(express.json());
+app.use(cors());
 app.use("/v1", require("./routes/v1/jokeRoutes"));
 
 swaggerDocs(app);
