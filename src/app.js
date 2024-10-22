574ff37361ecd0587fecc14a314e8ef659ae0373
Raphaël Beaugiraud <raphael.bgrd@gmail.com>
2024-10-22 16:13:13 +0200

    add info response to root level

--- src/app.js
+++ src/app.js
@@ -11,6 +11,20 @@ app.use("/v1", require("./routes/v1/jokeRoutes"));
 
 swaggerDocs(app);
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the Carambar & Co Jokes API",
+    version: "1.0.0",
+    endpoints: {
+      "POST /v1/jokes": "Create a new joke",
+      "GET /v1/jokes": "Retrieve all jokes",
+      "GET /v1/jokes/random": "Get a random joke",
+      "GET /v1/jokes/{id}": "Get a joke by ID",
+      "GET /api-docs": "Documentation",
+    },
+  });
+});
+
 app.use((req, res) => {
   res.status(404).send("404 Not found");
 });
@@ -18,6 +32,5 @@ app.use((req, res) => {
 app.listen(PORT, async () => {
   console.info(`Server is running at ${url}`);
   console.log(`Docs available at ${url}/api-docs`);
-  console.log(process.env.RENDER_EXTERNAL_URL);
   await connect();
 });
